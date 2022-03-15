bf19c301a3157d7b5054ab8404b8cd1d5122c41b
arthurmouradossantos <arthurpoliengenharia@gmail.com>
2022-03-15 09:13:53 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
